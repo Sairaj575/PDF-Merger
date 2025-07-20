74b76be7131c79a8ee69731ef8f222463814f4ab
Sairaj Umbarkar <147169758+Sairaj575@users.noreply.github.com>
2025-07-20 17:17:59 +0530

    pdf merger

---
+++ main.py
@@ -0,0 +1,21 @@
+from PyPDF2 import PdfWriter
+
+merger = PdfWriter()
+
+pdfs=[]
+try:
+    n = int(input("How many PDF do u wanna merge?\n"))
+
+    for i in range (0,n) :
+        name = input(f"Enter the name of PDF {i+1} : ")
+        pdfs.append(name)
+except:
+    print(f"----------invalid number----------")
+ 
+try :
+    for pdf in pdfs:
+        merger.append(pdf)
+except Exception as e:
+    print(f"---------------File not found: {e}------------")
+merger.write("merged-pdf4.pdf")
+merger.close()
