cb1102dbf1114ac043b5b9faac5d0473fadcd12f
Sairaj575 <sairajumbarkar575@gmail.com>
2025-07-20 17:28:30 +0530

    adjust spaces

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from PyPDF2 import PdfWriter
-
 merger = PdfWriter()
-
 pdfs=[]
 try:
     n = int(input("How many PDF do u wanna merge?\n"))
@@ -11,11 +9,12 @@ try:
         pdfs.append(name)
 except:
     print(f"----------invalid number----------")
- 
+
 try :
     for pdf in pdfs:
         merger.append(pdf)
 except Exception as e:
     print(f"---------------File not found: {e}------------")
+
 merger.write("merged-pdf4.pdf")
 merger.close()
